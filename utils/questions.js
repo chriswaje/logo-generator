f84fded2268fe0cc2c1c740360d022c8d9ebd42a
Chris Waje <chriswaje324@gmail.com>
2023-11-03 04:21:38 -0700

    ADDED: installed npm packages and created inquirer questions

---
+++ utils/questions.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Please enter text for the logo (Max of 3 characters)'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color for the text'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Please select a background shape',
+        choices: ['Triangle', 'Square', 'Circle']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color for the background shape'
+    },
+]
